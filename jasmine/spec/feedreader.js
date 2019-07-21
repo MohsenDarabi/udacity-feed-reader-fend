c8a941d19a08ca958cddf740472c1371f9c9c503
MohsenDarabi <mohsen.darabi@outlook.de>
2019-07-21 02:45:23 +0200

    Code first two tests for allFeeds object

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,7 +31,12 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-
+        it('URL is defined, is not empty', function() {
+            for (i = 0; i < allFeeds.length; i++) {
+              expect(allFeeds[i].url).toBeDefined();
+              expect(allFeeds[i].url.length).not.toBe(0);
+            }
+          });
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
@@ -39,6 +44,14 @@ $(function() {
          */
     });
 
+    it('name is defined, is not empty', function() {
+        for (i = 0; i < allFeeds.length; i++) {
+          expect(allFeeds[i].name).toBeDefined();
+          expect(allFeeds[i].name.length).not.toBe(0);
+        }
+      });
+
+ });
 
     /* TODO: Write a new test suite named "The menu" */
 
